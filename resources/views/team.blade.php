07fb775be306acd4854ce2583158b955ed716de5
hita-fttk <fttkmcc77@gmail.com>
2022-04-13 00:25:53 +0900

    a little change

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -37,7 +37,7 @@
           <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/603x363">
           <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">チーム名</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 s</h1>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 </h1>
             <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
         </div>
